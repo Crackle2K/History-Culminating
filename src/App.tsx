6ea4d5fff48b246537ad95a9e3dc32f153334393
Crackle2K <crackleontwitch@gmail.com>
2025-12-15 09:45:28 -0500

    Installed tailwind & shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     <>
-      
+      <h1>Top 10 Most Significant Events of Canadian and World History</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
